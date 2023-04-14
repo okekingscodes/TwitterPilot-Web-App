3fc9d22bfdc2c0da00d5e1fc1f8d78ced0e613bd
Odim Kingsley <okekings@gmail.com>
2023-04-14 01:40:54 +0100

    Hot fix

---
+++ mainjs.js
@@ -0,0 +1,46 @@
+// Script for drop now link
+/*
+function myFunction() {
+    var x = document.getElementById("policy");
+        if (x.className.indexOf("drop") == -1) {
+            x.className += "drop";
+        } else {
+            x.className = x.className.replace("drop", "");
+        }
+}
+*/
+
+
+
+
+//alert("Do you agreed to our Terms & Conditions?")
+/*
+fetch()
+    .then(res=> {
+        console.log(res);
+    })
+
+    fetch()
+    .then(res=> {
+       return res.json();
+    })
+    .then(data=> {
+        console.log(data);
+    })
+    .catch(error => console.log(error));
+
+    fetch()
+    .then(res=> {
+       return res.json();
+    })
+    .then(data => {
+        data.forEach(user => {
+            const markup = `<li>${user.name}</li>`;
+
+            document.querySelector(`ul`).insertAdjacentElement(`beforeend`, markup);
+        });
+    })
+    .catch(error => console.log(error));
+    */
+
+
